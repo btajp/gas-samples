2c64363dab86f2db7ee4b94dba1fb1b9134752bb
Shintaro Okamura <shintaro.okamura@studist.jp>
2023-04-18 14:47:33 +0000

    各種サンプル用のプロジェクトを作成

---
+++ SheetSample/src/sheet1.ts
@@ -0,0 +1,8 @@
+// Compiled using undefined undefined (TypeScript 4.9.5)
+var greeter = function (person) {
+    return "Hello, ".concat(person);
+};
+function testGreeter() {
+    var user = 'Bob';
+    Logger.log(greeter(user));
+}
